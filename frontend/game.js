7f81c8aeb23a2c0bdf2e959310ab660a7345d0b6
Jena Khatri <188423494+Khatri-That-codes@users.noreply.github.com>
2025-12-06 16:20:12 +1200

    Add code for create() function - will load the rb21 sprite - enabled arrow keys - enabled bounce reduction and scale car size

--- frontend/game.js
+++ frontend/game.js
@@ -37,8 +37,18 @@ function preload(){
 }
 
 function create(){
-    //TODO: add car sprite
-    //TODO: enable keyboard controls
+    // center of the screen is 400, 300
+    // will need to adjust the dimensions in config if need to change
+    car = this.physics.add.sprite(400,300, "rb21")
+    
+    // adjusting the car size -- will change maybe
+    car.setScale(0.5)
+
+    //reducing the bounce
+    car.setCollideWorldBounds(true)
+
+    //getting the arrow keys
+    cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update(){
