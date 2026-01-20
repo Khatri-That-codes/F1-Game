2e0047d46a973f654e5b0b5f0a85855e735c8373
Jena Khatri <188423494+Khatri-That-codes@users.noreply.github.com>
2026-01-20 22:38:33 +1200

    Add popular lines from F1 teams and players

---
+++ src/dialogues.py
@@ -0,0 +1,46 @@
+'''
+This file contains popular lines said by teams and players in F1
+'''
+## STILL NEED TO ADD MORE SAYINGS
+
+popular_lines_by_team = {
+    "Ferrari" : [
+        "Must be the water",
+        "I am stupid",
+        
+    ],
+
+    "Mercedes" : [
+        "I am the best driver in the world",
+        "I am unstoppable",
+        
+    ],
+
+    "Red Bull" : [
+        "Tu du du du du , Max Verstappen",
+        "",
+        
+    ],
+
+}
+
+popular_lines_by_players = {
+    "Lewis Hamilton" : [
+        "Still not over 7",
+        "My car is faster than yours",
+        
+    ],
+
+    "Max Verstappen" : [
+        "Max Attack!",
+        "I am the future of F1",
+        
+    ],
+
+    "Charles Leclerc" : [
+        "Forza Ferrari!",
+        "I will win for Ferrari",
+        
+    ],
+
+}
