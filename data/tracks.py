c13b7d23ac946687011ffc555ddbf0cd33218057
Jena Khatri <188423494+Khatri-That-codes@users.noreply.github.com>
2026-01-27 15:42:57 +1200

    add track class and all track dict - the tracks also make reference to the track images

---
+++ data/tracks.py
@@ -0,0 +1,33 @@
+"""
+Module for managing track data.
+"""
+
+from typing import Dict, List
+from assets.ascii.ascii_tracks import _ascii_tracks
+from data.weather import WEATHER_CONDITION
+from enum import Enum
+
+class Track:
+    def __init__(self, name: str, country: str, number_laps: int,  weather_options: WEATHER_CONDITION):
+        self.name = name
+        self.country = country
+        self.number_laps = number_laps
+        self.weather_options = weather_options
+
+      #neeed to addd check to replace with generic image if not found 
+        self.image_path = f"assets/tracks/{self.name.lower().replace(' ', '_')}.png"
+
+
+
+
+
+
+ALL_TRACKS: Dict[str, Track] = {
+    "suzuka": Track("Suzuka", "Japan", 53, [WEATHER_CONDITION.SUNNY, WEATHER_CONDITION.RAINY]),
+    "monza": Track("Monza", "Italy", 53, [WEATHER_CONDITION.SUNNY, WEATHER_CONDITION.CLOUDY]),
+    "spa" : Track("Spa", "Belgium", 44, [WEATHER_CONDITION.RAINY, WEATHER_CONDITION.CLOUDY]),
+    "monaco": Track("Monaco", "Monaco", 78, [WEATHER_CONDITION.SUNNY, WEATHER_CONDITION.STORMY]),
+    "silverstone": Track("Silverstone", "UK", 52, [WEATHER_CONDITION.SUNNY, WEATHER_CONDITION.CLOUDY]),
+
+    "generic track": Track("Generic Track", "Nowhere", 50, [WEATHER_CONDITION.SUNNY, WEATHER_CONDITION.RAINY, WEATHER_CONDITION.CLOUDY, WEATHER_CONDITION.STORMY])
+}
