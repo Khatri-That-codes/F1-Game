28240c370b325094d708305790335dd31095bc8c
Jena Khatri <188423494+Khatri-That-codes@users.noreply.github.com>
2026-01-30 16:36:39 +1200

    updat main.py to add standing router and allow credentials

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,18 +1,20 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-from backend.api.drivers import drivers_router
-from backend.api.teams import teams_router
-from backend.api.tracks import tracks_router
+from api.drivers import drivers_router
+from api.teams import teams_router
+from api.tracks import tracks_router
+from api.standings import standings_router
 
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"], 
     allow_methods=["*"],
     allow_headers=["*"],
+    allow_credentials=True, 
 )
 
 
@@ -20,3 +22,4 @@ app.add_middleware(
 app.include_router(drivers_router, prefix="/api")
 app.include_router(teams_router, prefix="/api")
 app.include_router(tracks_router, prefix="/api")
+app.include_router(standings_router)
